ce8a16c947ae7c90e68bd6664a1190289a7625c7
Jay Owings <jaydenowings@mail.weber.edu>
2024-10-16 11:11:25 -0600

    Game start logic

--- yahtzeetest.cpp
+++ yahtzeetest.cpp
@@ -7,5 +7,17 @@
 using namespace std;
 
 int main(){
-    
+    bool computer;
+    int numPlayers;
+    char cpu;
+    cout << "How many players? ";
+    cin >> numPlayers;
+    cout << "Computer player? Y/N ";
+    cin >> cpu;
+    if (cpu == 'N' || cpu == 'n'){
+        computer = false;
+    }else{
+        computer = true;
+    }
+    playYahtzee(numPlayers, computer);
 }
